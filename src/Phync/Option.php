3f70f6b8ec3b55fadbec36201fdf9105fcbc8fc1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-04-01 18:00:38 +0900

    Add comments.

--- src/Phync/Option.php
+++ src/Phync/Option.php
@@ -20,6 +20,11 @@ class Phync_Option
         $this->parse();
     }
 
+    /**
+     * 引数をパースする.
+     *
+     * @return void
+     */
     public function parse()
     {
         foreach ($this->options as $key => $value) {
@@ -31,21 +36,42 @@ class Phync_Option
         }
     }
 
+    /**
+     * 実行フラグをセットする.
+     *
+     * @param  bool $pred
+     * @return void
+     */
     public function setExecute($pred)
     {
         $this->dryRun = ((bool)$pred) === false;
     }
 
+    /**
+     * ドライランで実行するか.
+     *
+     * @return bool
+     */
     public function isDryRun()
     {
         return $this->dryRun;
     }
 
+    /**
+     * 引数にファイルが指定されているか.
+     *
+     * @return bool
+     */
     public function hasFiles()
     {
         return is_array($this->files) && count($this->files) > 0;
     }
 
+    /**
+     * 引数にファイルとして指定された文字列を全て取得する.
+     *
+     * @return array
+     */
     public function getFiles()
     {
         return $this->files;
