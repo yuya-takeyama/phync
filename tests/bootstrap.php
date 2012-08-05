d33b9ee37cdc39825514402170f6cb03e1eb5be6
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-05 17:40:44 +0900

    Add PEAR libs into vendor

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
-set_include_path(dirname(__FILE__) . '/../src' . PATH_SEPARATOR . get_include_path());
-ini_set('error_reporting', E_ALL);
+set_include_path(
+    dirname(__FILE__) . '/../src' . PATH_SEPARATOR .
+    dirname(__FILE__) . '/../vendor/pear' . PATH_SEPARATOR .
+    get_include_path()
+);
