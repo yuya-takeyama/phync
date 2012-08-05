348b89abb1fc0bb868b3b77e10753a05a66ad509
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-05 19:07:24 +0900

    Add Phync_FileUtil class

--- src/Phync/CommandGenerator.php
+++ src/Phync/CommandGenerator.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * This file is part of Phync.
+ *
+ * (c) Yuya Takeyama
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'File/Util.php';
+
+/**
+ * rsync command generator.
+ *
+ * @author Yuya Takeyama
+ */
 class Phync_CommandGenerator
 {
     public function getCommands(Phync_Config $config, Phync_Option $option)
