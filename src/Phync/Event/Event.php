c7727e21ee330f5b4a91c0c936b50e99b00399ef
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-12 06:57:28 +0900

    Add comment

--- src/Phync/Event/Event.php
+++ src/Phync/Event/Event.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of Phync.
+ *
+ * (c) Yuya Takeyama
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 class Phync_Event_Event
 {
     private $name;
--- tests/Phync/Tests/OptionTest.php
+++ tests/Phync/Tests/OptionTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of Phync.
+ *
+ * (c) Yuya Takeyama
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once 'Phync/Option.php';
 
 class Phync_Tests_OptionTest extends Phync_Tests_TestCase
