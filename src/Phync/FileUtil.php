d1f9239e65c3a2865fa3aded42756c8bf9a179a9
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-12 18:33:33 +0900

    Add Phync_FileUtil->getCwd() method

--- src/Phync/FileUtil.php
+++ src/Phync/FileUtil.php
@@ -36,4 +36,19 @@ class Phync_FileUtil
     {
         return escapeshellarg($arg);
     }
+
+    /**
+     * カレントワーキングディレクトリを取得する
+     *
+     * カレントディレクトリがシンボリックリンクだった場合、そのシンボリックリンク自体のパスを返す
+     * PHP の getcwd() ではシンボリックリンクが解決されてしまうので、その代替
+     *
+     * @TODO OS によって挙動が違わないかの調査
+     *
+     * @return string
+     */
+    public function getCwd()
+    {
+        return chop(`pwd`);
+    }
 }
